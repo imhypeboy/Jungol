4e43f1e64dba0291c6b832db10cfa0e4f0323b4e
imhypeboy <kangjh0205@gmail.com>
2024-12-31 15:02:52 +0900

    구조체 - 자가진단1 #613

--- Jungol/src/test/test613.java
+++ Jungol/src/test/test613.java
@@ -1,24 +1,23 @@
 package test;
 import java.util.Scanner;
 
-public class test613 {
+public class test613{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // 이름, 학교명, 학년 입력받기
         System.out.print(" ");
-        String name = scanner.next(); // 이름 입력
+        String name = scanner.next(); // 이름 
 
         System.out.print(" ");
-        String school = scanner.next(); // 학교명 입력
+        String school = scanner.next(); // 학교
 
         System.out.print(" ");
-        int grade = scanner.nextInt(); // 학년 입력
+        int grade = scanner.nextInt(); // 학년
 
         // 출력
-        System.out.println("\nName   : " + name);
+        System.out.println("\nName : " + name);
         System.out.println("School : " + school);
-        System.out.println("Grade  : " + grade);
+        System.out.println("Grade : " + grade);
 
         scanner.close(); // 스캐너 닫기
     }
