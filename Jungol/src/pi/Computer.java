a3b467e11ad8f65f882385234df46618f507f066
imhypeboy <kangjh0205@gmail.com>
2025-01-02 12:39:45 +0900

    PI계

---
+++ Jungol/src/pi/Computer.java
@@ -0,0 +1,8 @@
+package pi;
+public class Computer extends Calculator {
+    @Override
+    double areaCircle(double r) {
+        System.out.println("Computer 객체의 areaCircle() 실행");
+        return Math.PI * r * r;
+    }
+}
