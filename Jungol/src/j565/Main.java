999bb4abb7393ea6ae989e87e3408ef93188f0a8
Jh <kangjh0205@gmail.com>
2025-02-20 09:48:09 +0900

    jungol j565

---
+++ Jungol/src/j565/Main.java
@@ -0,0 +1,18 @@
+package j565;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		
+	}
+}
+
+
+/*100 미만의 양의 정수들이 주어진다. 
+
+입력받다가 0 이 입력되면 마지막에 입력된 0 을 제외하고 
+
+그 때까지 입력된 정수의 십의 자리 숫자가 각각 몇 개인지 
+
+작은 수부터 출력하는 프로그램을 작성하시오. (0개인 숫자는 출력하지 않는다.)*/
